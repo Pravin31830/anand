65fa9bac6ae3338de0e3205a6409daf70eff0bde
ananth <ananth.skp@gmail.com>
2024-09-03 19:59:02 +0530

    CHANGES

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,22 @@
+
+  /** @type {import('tailwindcss').Config} */
 module.exports = {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    }
-  }
+  content: [
+    './pages/**/*.{html,js,jsx}',
+    './components/**/*.{html,js,jsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#84cc16'
+      }
+    },
+    fontFamily: {
+      'logo':["Kaushan Script"],
+      'primary': ["Monsterrat", 'sans-serif'],
+      'secondary':["Poppins", 'sans-serif']
+    },
+    
+  },
+  plugins: [],
+}
